a3fdf41e10b8fefcc6323abbacd5b93f9f8b509a
viktor <viktorvanyatanya@gmail.com>
2022-09-19 21:46:22 +0200

    lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
+import {RouterModule} from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {UserResolveService} from './services/user.resolve.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostsComponent,
+    CommentsComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'users',
+        component: UsersComponent,
+        children: [
+          {path: ':id', component: UserDetailsComponent, resolve: {data: UserResolveService}},
+        ]
+      },
+      // {path: 'users/:id', component: UserDetailsComponent},
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -3,4 +3,21 @@ export interface User {
   name: string;
   email: string;
   phone: string;
+  username: string;
+  website: string;
+  address: {
+    city: string;
+    geo: {
+      lat: string;
+      lng: string;
+    }
+    street: string;
+    suite: string;
+    zipcode: string;
+  }
+  company: {
+    bs: string;
+    name: string;
+    catchPhrase: string;
+  }
 }
